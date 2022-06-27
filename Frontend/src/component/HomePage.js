cb95ed73fda4f2fdf73c7dc4dc2e17e0d17ed5b7
Nguyễn Văn Sáng <nguyensanggb@gmail.com>
2022-06-28 00:44:20 +0700

    Fix display product

--- Frontend/src/component/HomePage.js
+++ Frontend/src/component/HomePage.js
@@ -19,27 +19,27 @@ export default function HomePage(){
                             </div>
                             <div className="container-productbox">
                                 <ul className="list-product">
-                                    <li className="iteam_box">
                                         {
                                             products.filter((v, i) => {
                                                 console.log(typeof(v.name));
                                                 return v.name?.toLowerCase().includes(category);
                                             }).map((v, i) => {
                                                 return (
-                                                    <a href='' className="main_box">
-                                                        <div className="item-img item-img_42">
-                                                            <img className='' src={v.img} width="207" height="207"></img>
-                                                        </div>
-                                                        <h3>
-                                                            {v.name}
-                                                        </h3>
-                                                        <strong className='price'>Giá từ: {v.price}₫</strong>
-                                                        <p className="item-where">Có tất cả : {v.count} nơi bán</p>
-                                                    </a>
+                                                    <li className="iteam_box">
+                                                        <a href='' className="main_box">
+                                                            <div className="item-img item-img_42">
+                                                                <img className='' src={v.img} width="207" height="207"></img>
+                                                            </div>
+                                                            <h3>
+                                                                {v.name}
+                                                            </h3>
+                                                            <strong className='price'>Giá từ: {v.price}₫</strong>
+                                                            <p className="item-where">Có tất cả : {v.count} nơi bán</p>
+                                                        </a>
+                                                    </li>
                                                 )
                                             })
                                         }    
-                                    </li>
                                 </ul>
                             </div>
                         </section>
